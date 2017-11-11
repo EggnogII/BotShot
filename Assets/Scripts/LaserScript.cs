c3ee1522dc9530d1fe7d250d14b1f4dd386bc1e7
Imran <imranbirfan@gmail.com>
2017-11-10 16:44:25 -0800

    Added LaserScript, currently making Fire Function and Update

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour {
+
+	public float mFireRate = 0.5f;
+	public float mFireRange = 50f;
+	public float mHitForce = 100f;
+	public int mLaserDamage = 100;
+
+	//Line render that will represent the Laser
+	private LineRenderer mLaserLine;
+	
+	//Define if laser line is showing
+	private bool mLaserLine;
+	
+	//Time Laser Line shows onScreen
+	private WaitForSeconds mLaserDuration = new WaitForSeconds(0.05f);
+	
+	//Time until next fire
+	private float mNextFire;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		//Get Line Renderer
+		mLaserLine = GetComponent<LineRenderer>();
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//FIRE FUNCTION GOES HERE
+}
